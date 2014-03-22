a753d21fed90386fcb9390a23157bb7805c2286b
Leslie A. Schultz <leslie.a.schultz@gmail.com>
2014-03-22 16:23:49 -0500

    privatization

--- about-avail/documentation/private/library-documentation/controllers.js
+++ about-avail/documentation/private/library-documentation/controllers.js
@@ -5,9 +5,33 @@
 var methodListApp = angular.module('methodListApp', []);
 
 methodListApp.controller('MethodListCtrl', function($scope, $http) {
-  $http.get('method-data-files/tuple-methods.json').success(function(data) {
+  $http.get('method-data-files/methods.json').success(function(data) {
     $scope.methods = data;
   });
   $scope.orderProp = 'name';
   $scope.queryBy='name';
 });
+
+var categoryListApp = angular.module('categoryListApp', []);
+
+categoryListApp.controller('CategoryListCtrl', function($scope, $http) {
+  $http.get('category-data-files/categories.json').success(function(data) {
+    $scope.categories = data;
+  });
+  $scope.orderProp = 'name';
+  $scope.queryBy='name';
+});
+
+/*'use strict';
+
+ Controllers 
+
+var methodListApp = angular.module('methodListApp', []);
+
+methodListApp.controller('MethodListCtrl', function($scope, $http) {
+  $http.get('method-data-files/tuple-methods.json').success(function(data) {
+    $scope.methods = data;
+  });
+  $scope.orderProp = 'name';
+  $scope.queryBy='name';
+});*/
